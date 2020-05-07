2e3bc5b52f244f285da82d664060f4fd843b03ed
wilsonyin <15055495@qq.com>
2020-05-07 23:12:20 +0800

    1a

--- 1a.py
+++ 1a.py
@@ -1,1 +1,15 @@
-1a
+# 使用requests库获取豆瓣影评
+
+import requests
+
+
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+
+header = {'user-agent':user_agent}
+
+myurl = 'https://movie.douban.com/top250'
+
+response = requests.get(myurl,headers=header)
+
+print(response.text)
+print(f'返回码是: {response.status_code}')
