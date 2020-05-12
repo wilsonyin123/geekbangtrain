1f0ae08631a039e1760acf50e4048cbc3c573f3a
wilsonyin <15055495@qq.com>
2020-05-13 00:30:36 +0800

    2a

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
-翻页的处理
-文件保存
+Scrapy爬虫初步
+
+安装步骤：
+1 pip install scrapy
+2 scrapy startproject doubanmovie
+3 ./doubanmovie/doubanmovie/spiders
+
---
+++ spider1/doubanmovie/items.py
@@ -0,0 +1,18 @@
+
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanmovieItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    # 注释原有的pass
+    # pass
+    title = scrapy.Field()
+    link = scrapy.Field()
