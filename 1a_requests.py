65fb14ca4738ea9064db604465e371c491bf9ed3
wilsonyin <15055495@qq.com>
2020-05-16 15:55:34 +0800

    1a

--- 1a_requests.py
+++ 1a_requests.py
@@ -1,6 +1,7 @@
 # 使用requests库获取豆瓣影评
 
 import requests
+import urllib
 
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
@@ -10,6 +11,9 @@ header = {'user-agent':user_agent}
 myurl = 'https://movie.douban.com/top250'
 
 response = requests.get(myurl,headers=header)
+
+# resp = urllib.request.urlopen('myurl')
+# print(resp.read().decode())
 
 print(response.text)
 print(f'返回码是: {response.status_code}')
