8a3477c506d98756fd7aad5028b63540dd0115ef
wilsonyin <15055495@qq.com>
2020-05-24 17:32:39 +0800

    3a

---
+++ proxyspider/proxyspider/spiders/httpbin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+# export http_proxy='http://52.179.231.206:80'
+# setting 增加 scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware
+# 通过 Request.meta['proxy'] 读取 http_proxy 环境变量加载代理
+
+
+class HttpbinSpider(scrapy.Spider):
+    name = 'httpbin'
+    allowed_domains = ['httpbin.org']
+    # 通过ip查看请求的ip地址
+    start_urls = ['http://httpbin.org/ip']
+    # 通过header 查看user-agent
+    # start_urls = ['http://httpbin.org/headers']
+
+    def parse(self, response):
+        print(response.text)
+
