e00fcf329ebbcf4bd0f95eeeeb1b9b7e354cf380
wilsonyin <15055495@qq.com>
2020-05-26 21:43:56 +0800

    3c

--- miniScrapy.py
+++ miniScrapy.py
@@ -3,53 +3,46 @@ from lxml import etree
 from queue import Queue
 import threading
 import json
-'''
-Queue.qsize(队列名) #返回队列的大小
-Queue.empty(队列名) # 队列为空返回true，否则为false
-Queue.full(队列名) # 队列满返回true
-Queue.get(队列名,值) # 出队
-Queue.put(队列名,值) # 入队
-FIFO 先进先出
-'''
 
-class Crawl_thread(threading.Thread):
+class CrawlThread(threading.Thread):
     '''
-    抓取线程类，注意需要继承线程类Thread
+    爬虫类
     '''
     def __init__(self,thread_id,queue):
-        threading.Thread.__init__(self) # 需要对父类的构造函数进行初始化
+        super().__init__() 
         self.thread_id = thread_id  
-        self.queue = queue # 任务队列
+        self.queue = queue
 
     def run(self):
         '''
-        线程在调用过程中就会调用对应的run方法
-        :return:
+        重写run方法
         '''
-        print('启动线程：',self.thread_id)
-        self.crawl_spider()
-        print('退出了该线程：',self.thread_id)
+        print(f'启动线程：{self.thread_id}')
+        self.scheduler()
+        print(f'结束线程：{self.thread_id}')
 
-    def crawl_spider(self):
+    # 模拟任务调度
+    def scheduler(self):
         while True:
-            if self.queue.empty(): #如果队列为空，则跳出
+            if self.queue.empty(): #队列为空不处理
                 break
             else:
                 page = self.queue.get()
-                print('当前工作的线程为：',self.thread_id," 正在采集：",page)
-                url = 'https://www.qiushibaike.com/Shr/page/{}/'.format(str(page))
+                print('下载线程为：',self.thread_id," 下载页面：",page)
+                url = f'https://book.douban.com/top250?start={page*25}'
                 headers = {
-                    'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3371.0 Safari/537.36'
+                    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
                 }
                 try:
-                    content = requests.get(url,headers=headers)
-                    data_queue.put(content.text) # 将采集的结果放入data_queue中
+                    # downloader 下载器
+                    response = requests.get(url,headers=headers) 
+                    dataQueue.put(response.text)
                 except Exception as e:
-                    print('采集线程错误',e)
+                    print('下载出现异常',e)
 
-class Parser_thread(threading.Thread):
+class ParserThread(threading.Thread):
     '''
-    解析网页的类，就是对采集结果进行解析，也是多线程方式进行解析
+    页面内容分析
     '''
     def __init__(self,thread_id,queue,file):
         threading.Thread.__init__(self)
@@ -58,17 +51,18 @@ class Parser_thread(threading.Thread):
         self.file = file
 
     def run(self):
-        print('启动线程：', self.thread_id)
+        print(f'启动线程：{self.thread_id}')
         while not flag:
             try:
-                item = self.queue.get(False) # get参数为false时队列为空，会抛出异常
+                item = self.queue.get(False) # 参数为false时队列为空，抛出异常
                 if not item:
                     pass
                 self.parse_data(item)
-                self.queue.task_done() # 每当发出一次get操作，就会提示是否堵塞
+                self.queue.task_done() # get之后检测是否会阻塞
             except Exception as e:
                 pass
-        print('退出了该线程：', self.thread_id)
+        print(f'结束线程：{self.thread_id}')
+
     def parse_data(self,item):
         '''
         解析网页内容的函数
@@ -77,65 +71,66 @@ class Parser_thread(threading.Thread):
         '''
         try:
             html = etree.HTML(item)
-            result = html.xpath('//div[contains(@id,"qiushi_tag")]') # 匹配所有段子内容
-            for site in result:
+            books = html.xpath('//div[@class="pl2"]')
+            for book in books:
                 try:
-                    img_url = site.xpath('.//img/@src')[0] # 糗事图片
-                    title = site.xpath('.//h2')[0].text # 糗事题目
-                    content = site.xpath('.//div[@class="content"]/span')[0].text.strip() # 糗事内容
+                    title = book.xpath('./a/text()')
+                    link = book.xpath('./a/@href')
                     response={
-                        'img_url':img_url,
                         'title':title,
-                        'content':content
-                    } #构造json
-                    json.dump(response,fp=self.file,ensure_ascii=False) # 存放json文件
+                        'link':link
+                    } 
+                    #解析方法和scrapy相同，再构造一个json
+                    json.dump(response,fp=self.file,ensure_ascii=False) 
                 except Exception as e:
-                    print('parse 2: ', e)
+                    print('book error', e)
 
         except Exception as e:
-            print('parse 1: ',e)
+            print('page error',e)
 
 
-data_queue = Queue() # 存放解析数据的queue
+dataQueue = Queue() # 存放解析数据的queue
 flag = False
+
 def main():
-    output = open('qiushi.json','a',encoding='utf-8') # 将结果保存到一个json文件中
-    pageQueue = Queue(50) # 任务队列，存放网页的队列
-    for page in range(1,11): 
-        pageQueue.put(page) # 构造任务队列
-    # 初始化采集线程
+    # 将结果保存到一个json文件中
+    output = open('book.json','a',encoding='utf-8') 
+
+    # 任务队列，存放网页的队列
+    pageQueue = Queue(50) 
+    for page in range(0,11): 
+        pageQueue.put(page) 
+    
+    # 爬虫线程
     crawl_threads = []
-    crawl_name_list = ['crawl_1','crawl_2','crawl_3'] # 总共构造3个爬虫线程
+    crawl_name_list = ['crawl_1','crawl_2','crawl_3'] 
     for thread_id in crawl_name_list:
-        thread = Crawl_thread(thread_id,pageQueue) # 启动爬虫线程
-        thread.start() # 启动线程
+        thread = CrawlThread(thread_id,pageQueue)
+        thread.start() 
         crawl_threads.append(thread)
-    # 初始化解析线程
+    
+    # 解析线程
     parse_thread = []
     parser_name_list = ['parse_1','parse_2','parse_3']
-    for thread_id in parser_name_list: # 
-        thread = Parser_thread(thread_id,data_queue,output)
-        thread.start() # 启动线程
+    for thread_id in parser_name_list: 
+        thread = ParserThread(thread_id,dataQueue,output)
+        thread.start() 
         parse_thread.append(thread)
 
-    # 等待队列情况，先进行网页的抓取
-    while not pageQueue.empty(): # 判断是否为空
-        pass # 不为空，则继续阻塞
 
-    # 等待所有线程结束
+    # 结束crawl线程
     for t in crawl_threads:
         t.join()
-    # 等待队列情况，对采集的页面队列中的页面进行解析，等待所有页面解析完成
-    while not data_queue.empty():
-        pass
-    # 通知线程退出
+    
+
+    # 结束parse线程
     global flag
     flag = True
     for t in parse_thread:
-        t.join() # 等待所有线程执行到此处再继续往下执行
+        t.join() 
 
-    print('退出主线程')
     output.close()
-
+    print('退出主线程')
+    
 if __name__ == '__main__':
     main()
