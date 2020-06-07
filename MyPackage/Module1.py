0eb5f3540b8a04d0268801827a9a678877b590b4
wilsonyin <15055495@qq.com>
2020-06-07 22:16:07 +0800

    5a

---
+++ MyPackage/Module1.py
@@ -0,0 +1,2 @@
+def func1():
+    print('import func1')
--- 禁断的魔术/readcsv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-filename = 'book.csv'
-with open(filename, "r", encoding='utf-8') as f:
-    for line in f:
-        print(line)
-
